c0d48f662828ea0dd071da5575f95b81c628bb92
Kiersten <kiersten.schmidt@nbcuniversal.com>
2019-07-01 17:26:12 -0400

    chart created

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,16 @@
 
 // console.log(Highcharts);
 
+Highcharts.setOptions({
+    lang: {
+      thousandsSep: ','
+    }
+});
 
 document.addEventListener('DOMContentLoaded', function () {
     const myChart = Highcharts.chart('chart-container', {
         chart: {
-            type: 'bar',
+            type: 'column',
             styledMode: true,
             spacingBottom: 25,
             spacingRight: 100
@@ -16,21 +21,39 @@ document.addEventListener('DOMContentLoaded', function () {
         title: {
             text: null
         },
+        data: {
+            googleSpreadsheetKey: '1eM4oCpHRWZ5NJi2oCqpXyK2CsBJtWOOSpfgc-oSceoI',
+            googleSpreadsheetWorksheet: 2,
+            endColumn: 5,
+            startRow: 30
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
         legend: {
             align: 'right',
             symbolRadius: 0,
             verticalAlign: 'top',
             x: 10,
-            itemMarginTop: -10
+            itemMarginTop: -10,
+            reversed: true
         },
         xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
+            labels: {
+                autoRotation: false
+            },
+            tickLength: 5
         },
         yAxis: {
             title: false,
             labels: {
                 useHTML: true,
-                overflow: 'allow'
+                overflow: 'allow',
+                formatter: function() {
+                    return (this.value/1000).toFixed(1);
+                }
             }
         },
         credits: {
@@ -38,7 +61,11 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         tooltip: {
             shadow: false,
-            padding: 10
+            padding: 10,
+            valuePrefix: '$',
+            valueSuffix: ' billion',
+            valueDecimals: 1,
+            shared: true
         },
         responsive: {
             rules: [{
@@ -51,20 +78,106 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
                 legend: {
                     align: 'left',
-                    x: -18
+                    x: -18,
+                    itemMarginTop: 0
                 },
                 tooltip: {
                     enabled: false
                 }
               }
             }]
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const myChart = Highcharts.chart('chart-container-2', {
+        chart: {
+            type: 'line',
+            styledMode: true,
+            spacingBottom: 25,
+            spacingRight: 100
+        }, 
+        title: {
+            text: null
         },
-        series: [{
-            name: 'Jane',
-            data: [1, 6, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
+        data: {
+            googleSpreadsheetKey: '1eM4oCpHRWZ5NJi2oCqpXyK2CsBJtWOOSpfgc-oSceoI',
+            googleSpreadsheetWorksheet: 3,
+            endColumn: 4
+        },
+        plotOptions: {
+            series: {
+                lineWidth: 3,
+                clip: false,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    fillColor: '#ffffff',
+                    states: {
+                        hover: {
+                            fillColor: '#ffffff'
+                        }
+                    }
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            symbolRadius: 0,
+            symbolWidth: 0,
+            reversed: true,
+            layout: 'proximate',
+            align: 'right',
+            padding: 0
+        },
+        xAxis: {
+            labels: {
+                autoRotation: false,
+                formatter: function () {
+                    let str = this.value;
+                    return str.substr(3,6);
+                },
+            },
+            tickInterval: 8
+        },
+        yAxis: {
+            title: false,
+            floor: 0,
+            labels: {
+                useHTML: true,
+                overflow: 'allow'
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        tooltip: {
+            shadow: false,
+            padding: 10,
+            valueSuffix: '%',
+            valueDecimals: 1,
+        },
+        responsive: {
+            rules: [{
+              condition: {
+                maxWidth: 500
+              },
+              chartOptions: {
+                chart: {
+                  spacingRight: 10
+                },
+                legend: {
+                    itemMarginTop: 0
+                },
+                tooltip: {
+                    enabled: false
+                },
+                xAxis: {
+                    tickInterval: 4 
+                }
+              }
+            }]
+        }
     });
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,20 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 // import Highcharts from 'highcharts';
 // import Exporting from 'highcharts/modules/exporting';
 // Exporting(Highcharts);
 // console.log(Highcharts);
+Highcharts.setOptions({
+  lang: {
+    thousandsSep: ','
+  }
+});
 document.addEventListener('DOMContentLoaded', function () {
   var myChart = Highcharts.chart('chart-container', {
     chart: {
-      type: 'bar',
+      type: 'column',
       styledMode: true,
       spacingBottom: 25,
       spacingRight: 100
@@ -15,21 +22,39 @@ document.addEventListener('DOMContentLoaded', function () {
     title: {
       text: null
     },
+    data: {
+      googleSpreadsheetKey: '1eM4oCpHRWZ5NJi2oCqpXyK2CsBJtWOOSpfgc-oSceoI',
+      googleSpreadsheetWorksheet: 2,
+      endColumn: 5,
+      startRow: 30
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal'
+      }
+    },
     legend: {
       align: 'right',
       symbolRadius: 0,
       verticalAlign: 'top',
       x: 10,
-      itemMarginTop: -10
+      itemMarginTop: -10,
+      reversed: true
     },
     xAxis: {
-      categories: ['Apples', 'Bananas', 'Oranges']
+      labels: {
+        autoRotation: false
+      },
+      tickLength: 5
     },
     yAxis: {
       title: false,
       labels: {
         useHTML: true,
-        overflow: 'allow'
+        overflow: 'allow',
+        formatter: function formatter() {
+          return (this.value / 1000).toFixed(1);
+        }
       }
     },
     credits: {
@@ -37,7 +62,11 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     tooltip: {
       shadow: false,
-      padding: 10
+      padding: 10,
+      valuePrefix: '$',
+      valueSuffix: ' billion',
+      valueDecimals: 1,
+      shared: true
     },
     responsive: {
       rules: [{
@@ -50,20 +79,105 @@ document.addEventListener('DOMContentLoaded', function () {
           },
           legend: {
             align: 'left',
-            x: -18
+            x: -18,
+            itemMarginTop: 0
           },
           tooltip: {
             enabled: false
           }
         }
       }]
+    }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var _legend;
+
+  var myChart = Highcharts.chart('chart-container-2', {
+    chart: {
+      type: 'line',
+      styledMode: true,
+      spacingBottom: 25,
+      spacingRight: 100
     },
-    series: [{
-      name: 'Jane',
-      data: [1, 6, 4]
-    }, {
-      name: 'John',
-      data: [5, 7, 3]
-    }]
+    title: {
+      text: null
+    },
+    data: {
+      googleSpreadsheetKey: '1eM4oCpHRWZ5NJi2oCqpXyK2CsBJtWOOSpfgc-oSceoI',
+      googleSpreadsheetWorksheet: 3,
+      endColumn: 4
+    },
+    plotOptions: {
+      series: {
+        lineWidth: 3,
+        clip: false,
+        marker: {
+          enabled: false,
+          symbol: 'circle',
+          fillColor: '#ffffff',
+          states: {
+            hover: {
+              fillColor: '#ffffff'
+            }
+          }
+        }
+      }
+    },
+    legend: (_legend = {
+      align: 'right',
+      symbolRadius: 0,
+      symbolWidth: 0,
+      reversed: true,
+      layout: 'proximate'
+    }, _defineProperty(_legend, "align", 'right'), _defineProperty(_legend, "padding", 0), _legend),
+    xAxis: {
+      labels: {
+        autoRotation: false,
+        formatter: function formatter() {
+          var str = this.value;
+          return str.substr(3, 6);
+        }
+      },
+      tickInterval: 8
+    },
+    yAxis: {
+      title: false,
+      floor: 0,
+      labels: {
+        useHTML: true,
+        overflow: 'allow'
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    tooltip: {
+      shadow: false,
+      padding: 10,
+      valueSuffix: '%',
+      valueDecimals: 1
+    },
+    responsive: {
+      rules: [{
+        condition: {
+          maxWidth: 500
+        },
+        chartOptions: {
+          chart: {
+            spacingRight: 10
+          },
+          legend: {
+            itemMarginTop: 0
+          },
+          tooltip: {
+            enabled: false
+          },
+          xAxis: {
+            tickInterval: 4
+          }
+        }
+      }]
+    }
   });
 });
